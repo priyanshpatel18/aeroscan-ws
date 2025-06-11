7096cb611e5bce354995532734ac4af007edc253
prianshpate18 <priyansh.180804@gmail.com>
2025-06-11 23:39:17 +0530

    updated queue .env

--- src/utils/pushToQueue.ts
+++ src/utils/pushToQueue.ts
@@ -1,7 +1,7 @@
 import { redis } from "../db/redis";
 import { Queue } from "bullmq";
 
-const REDIS_QUEUE_NAME = process.env.WAITING_QUEUE_TEST || "waiting-queue";
+const REDIS_QUEUE_NAME = process.env.WAITING_QUEUE || "waiting-queue";
 
 export const waitingQueue = new Queue(REDIS_QUEUE_NAME, {
   connection: redis
